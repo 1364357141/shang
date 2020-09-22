3da67aedbc8ac17fc43eb015655c6d1f5ea2dfe9
陈星 <1364357141@qq.com>
2020-09-22 14:14:25 +0800

    diyici

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="klli">登录组件</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang='scss' scoped>
+    .klli {
+        background-color: #2b4b6b;
+        height: 100%;
+    }
+</style>
